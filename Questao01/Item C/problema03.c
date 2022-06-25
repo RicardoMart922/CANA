f2fb42c236b59b510d1079f276f5c928f005e51f
Ricardo Martins <ricardo.martins.cordeiro.922@gmail.com>
2022-06-25 19:56:34 -0300

    Problema 03 resolvido pelo método de ordenação Counting Sort na linguagem C

--- Questao01/Item C/problema03.c
+++ Questao01/Item C/problema03.c
@@ -1,30 +1,125 @@
 /*
+* Instituo Federal do Ceará - IFCE
+* Campus: Tianguá
+* Curso: Bacharelado em Ciência da Computação
+* Disciplina: Construção e Análise de Algoritmos
+* Professor: Adonias Caetano de Oliveira
+* Assunto: Métodos de Ordenação
+*
+* Equipe:
+*       -> Francinilson Rodrigues Lima
+*       -> Ricardo Martins Cordeiro
+*        
 * PROBLEMA 03: Faça um programa que cadastre 10 números, ordene-os e em
 * seguida encontre e mostre (Dica: Use C, Python ou Ruby):
 *
 *   I  - O menor número e quantas vezes ele aparece no vetor;
 *   II - O maior número e quantas vezes ele aparece no vetor;
 *
-* Método do Insertion Sort foi utilizado para resolver esse problema.
+* Método do Counting Sort foi utilizado para resolver esse problema.
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-void insertionSort(int v[]) {
-    int i, j, x;
+int maiorElemento(int vetor[]) {
+    int maior = 0;
 
-    for (i = 1; i < 10; i++) {
-        x = v[i];
-        j = i - 1;
-
-        while (j >= 0 && v[j] > x) {
-            v[j+1] = v[j];
-            j--;
+    for (int i = 0; i < 10; i++) {
+        if (i == 0) {
+            maior = vetor[i];
+        } else {
+            if (vetor[i] > maior) {
+                maior = vetor[i];
+            }
         }
-
-        v[j+1] = x;
     }
+    
+    return maior;
+}
+
+int quantidadeMaiorElemento(int vetor[]) {
+    int qtde = 0;
+    int maior = maiorElemento(vetor);
+
+    for (int i = 0; i < 10; i++) {
+        if (vetor[i] == maior)
+            qtde++;
+    }
+    
+    return qtde;
+}
+
+int menorElemento(int vetor[]) {
+    int menor = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 0) {
+            menor = vetor[i];
+        } else {
+            if (vetor[i] < menor) {
+                menor = vetor[i];
+            }
+        }
+    }
+    
+    return menor;
+}
+
+int quantidadeMenorElemento(int vetor[]) {
+    int qtde = 0;
+    int menor = menorElemento(vetor);
+
+    for (int i = 0; i < 10; i++) {
+        if (vetor[i] == menor)
+            qtde++;
+    }
+    
+    return qtde;
+}
+
+void countingSort(int vetorA[], int n) {
+
+    int k = maiorElemento(vetorA);
+    int vetorB[n];
+    int vetorC[k+1];
+    int i = 0;
+
+    for (i = 0; i < n; i++) {
+        vetorB[i] = 0;
+    }
+
+    for (i = 0; i < k; i++) {
+        vetorC[i] = 0;
+    }
+
+    for (i = 0; i < n; i++) {
+        vetorC[vetorA[i]-1]++;
+    }
+
+    for (i = 1; i < k; i++) {
+        vetorC[i] += vetorC[i-1];
+    }
+
+    for (i = n-1; i >= 0; i--) {
+        vetorB[vetorC[vetorA[i]-1]-1] = vetorA[i];
+        vetorC[vetorA[i]-1]--;
+    }
+
+    for (i = 0; i < n; i++) {
+        vetorA[i] = vetorB[i];
+    }
+}
+
+void imprimirLinha() {
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+}
+
+void imprimirVetor(int vetor[]) {
+    for (int i = 0; i < 10; i++) {
+        printf("%d\t", vetor[i]);
+    }
+    printf("\n");
 }
 
 int main() {
@@ -34,28 +129,19 @@ int main() {
         printf("Informe o %dª numero: ", i+1);
         scanf("%d", &vetor[i]);
     }
-    printf("\n");
 
-    insertionSort(vetor);
+    imprimirLinha();
 
-    int menor = vetor[0];
-    int maior = vetor[9];
+    countingSort(vetor, 10);
 
-    int qtdeMenor = 0, qtdeMaior = 1;
+    imprimirVetor(vetor);
 
-    for (int j = 0; j < 10; j++) {
-        printf("%d\t", vetor[j]);
-        if (menor == vetor[j]) 
-            qtdeMenor++;
-        if (maior == vetor[j])
-            qtdeMaior++;
-    }
-    printf("\n");
+    imprimirLinha();
 
-    printf("O menor numero: %d\n", menor);
-    printf("Quantidade de vezes que ele aparece: %d\n", qtdeMenor);
-    printf("O maior numero: %d\n", maior);
-    printf("Quantidade de vezes que ele aparece: %d\n", qtdeMenor);
+    printf("O menor numero: %d\n", menorElemento(vetor));
+    printf("Quantidade de vezes que ele aparece: %d\n", quantidadeMenorElemento(vetor));
+    printf("O maior numero: %d\n", maiorElemento(vetor));
+    printf("Quantidade de vezes que ele aparece: %d\n", quantidadeMaiorElemento(vetor));
 
     return 1;    
 }
